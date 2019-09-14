cc5b7f430712f7a2aac3de90f466c60d72ec73b0
tsam <five4seven@hotmail.com>
2019-09-14 12:47:12 +0300

    jpa_start_with_branch_1

--- src/main/java/com/__1/demo/DemoApplication.java
+++ src/main/java/com/__1/demo/DemoApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
+    /**
+     * for commit and push
+     **/
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }

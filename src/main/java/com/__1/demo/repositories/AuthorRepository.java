b01d7b9b7392d0744816ae3932fbda90a928a364
tsam <five4seven@hotmail.com>
2019-09-14 13:44:45 +0300

    jpa_start_with_branch_2

--- src/main/java/com/__1/demo/repositories/AuthorRepository.java
+++ src/main/java/com/__1/demo/repositories/AuthorRepository.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package com.__1.demo.repositories;
 
 import org.springframework.data.repository.CrudRepository;
@@ -7,3 +8,13 @@ import com.__1.demo.model.Author;
 public interface AuthorRepository extends CrudRepository<Author, Long>{
 
 }
+=======
+package com.__1.demo.repositories;
+
+import com.__1.demo.model.Author;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AuthorRepository extends CrudRepository<Author, Long> {
+
+}
+>>>>>>> jpa_start_with_branch_2
--- src/main/java/com/__1/demo/repositories/BookRepository.java
+++ src/main/java/com/__1/demo/repositories/BookRepository.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package com.__1.demo.repositories;
 
 import org.springframework.data.repository.CrudRepository;
@@ -7,3 +8,13 @@ import com.__1.demo.model.Book;
 public interface BookRepository extends CrudRepository<Book, Long> {
 
 }
+=======
+package com.__1.demo.repositories;
+
+import com.__1.demo.model.Book;
+import org.springframework.data.repository.CrudRepository;
+
+public interface BookRepository extends CrudRepository<Book, Long> {
+
+}
+>>>>>>> jpa_start_with_branch_2
